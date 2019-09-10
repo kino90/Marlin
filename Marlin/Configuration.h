b61ee620d47fe615c76ec14755e49fc2f457d803
Luca Maroni <kinovale@hotmail.com>
2019-09-11 00:34:04 +0200

    Update steps for BMG clone

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -706,7 +706,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.96, 79.96, 404.27, 474.5 } /* { 80, 80, 4000, 500 } */
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.96, 79.96, 404.27, 422 } /* { 80, 80, 4000, 500 } */
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -989,7 +989,7 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR true //false
+#define INVERT_E0_DIR false
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
